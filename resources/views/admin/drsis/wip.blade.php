25bc20384dbbdbf771da8b3be3d681a0bc3a3244
Jhomar Izar <jhomar.izar@gmail.com>
2023-09-07 04:33:13 +0800

    fixed 3

--- resources/views/admin/drsis/wip.blade.php
+++ resources/views/admin/drsis/wip.blade.php
@@ -3,7 +3,7 @@
     <div class="card">
         <div class="card-header">
             <div style="text-align: center;"><br>
-                <h1>DEV WORK IN PROGRESS!<br><br> </h1>
+                <h1>DEVELOPER WORK IN PROGRESS!<br><br> </h1>
             </div>
         </div>
     @endsection

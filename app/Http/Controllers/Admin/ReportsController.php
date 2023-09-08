c90ac4a2571ff37c01086e8693fc185bab05e72e
Jhomar Izar <jhomar.izar@gmail.com>
2023-09-08 15:00:59 +0800

    packing list fix

--- app/Http/Controllers/Admin/ReportsController.php
+++ app/Http/Controllers/Admin/ReportsController.php
@@ -36,6 +36,7 @@ class ReportsController extends Controller
             $transactions = Transaction::where("rx_no", $request->rx_number)
                 ->where("asset_id", $request->asset_id)->simplePaginate($perPage);
         } else {
+            if($request->printField <= 2){
             switch (true) {
                 case $request->hospital_id != '' && $request->run_no != '' && ($request->startDate != '' || $request->endDate != ''):
                     $transactions = Transaction::where("hospital_id", $request->hospital_id)
@@ -49,7 +50,7 @@ class ReportsController extends Controller
                     $transactions = Transaction::where("hospital_id", $request->hospital_id)
                         ->where("asset_id", $request->asset_id)
                         ->where("run_no", $request->run_no)
-                        ->where("status","!=",1);
+                        ->simplePaginate($perPage);
                     break;
 
                 case $request->hospital_id != '' && $request->run_no == '' && ($request->startDate != '' || $request->endDate != ''):
@@ -78,12 +79,56 @@ class ReportsController extends Controller
                     break;
 
                 default:
-                if($request->printField <= 2){
                     $transactions = Transaction::where("asset_id", $request->asset_id)->simplePaginate($perPage);
-                } else {
-                    $transactions = Transaction::where("asset_id", $request->asset_id)->get();
-                }
                 break;
+            }
+        }
+            else {
+                switch (true) {
+                    case $request->hospital_id != '' && $request->run_no != '' && ($request->startDate != '' || $request->endDate != ''):
+                        $transactions = Transaction::where("hospital_id", $request->hospital_id)
+                            ->where("run_no", $request->run_no)
+                            ->where("asset_id", $request->asset_id)
+                            ->whereBetween("calibration_date", [$request->startDate, $request->endDate])
+                            ->get();
+                        break;
+
+                    case $request->hospital_id != '' && $request->run_no != '' && ($request->startDate == '' || $request->endDate == ''):
+                        $transactions = Transaction::where("hospital_id", $request->hospital_id)
+                            ->where("asset_id", $request->asset_id)
+                            ->where("run_no", $request->run_no)
+                            ->get();
+                        break;
+
+                    case $request->hospital_id != '' && $request->run_no == '' && ($request->startDate != '' || $request->endDate != ''):
+                        $transactions = Transaction::where("hospital_id", $request->hospital_id)
+                            ->where("asset_id", $request->asset_id)
+                            ->whereBetween("calibration_date", [$request->startDate, $request->endDate])
+                            ->get();
+                        break;
+
+                    case $request->hospital_id == '' && $request->run_no != '' && ($request->startDate != '' || $request->endDate != ''):
+                        $transactions = Transaction::where("run_no", $request->run_no)
+                            ->where("asset_id", $request->asset_id)
+                            ->whereBetween("calibration_date", [$request->startDate, $request->endDate])
+                            ->get();
+                        break;
+
+                    case $request->hospital_id == '' && $request->run_no == '' && ($request->startDate != '' || $request->endDate != ''):
+                        $transactions = Transaction::where("asset_id", $request->asset_id)
+                            ->whereBetween("calibration_date", [$request->startDate, $request->endDate])
+                            ->get();
+                        break;
+
+                    case $request->hospital_id != '' && $request->run_no == '' && ($request->startDate == '' || $request->endDate == ''):
+                        $transactions = Transaction::where("hospital_id", $request->hospital_id)
+                            ->where("asset_id", $request->asset_id)->get();
+                        break;
+
+                    default:
+                        $transactions = Transaction::where("asset_id", $request->asset_id)->get();
+                    break;
+                }
             }
         }
 
